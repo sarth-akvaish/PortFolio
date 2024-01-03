99522bd4ead7e3de9f103fe189b61ec1c8f2366a
Sarthak <sarthakvkumar@gmail.com>
2024-01-03 16:22:08 +0530

    Added social links

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -13,10 +13,10 @@ const Navbar = () => {
                     transition={{ duration: 0.5 }}
                     animate={{ opacity: 1, scale: 1 }}>Sarthak</motion.span>
                 <div className="social">
-                    <a href=""><img src="/facebook.png" alt="" /></a>
-                    <a href=""><img src="/instagram.png" alt="" /></a>
-                    <a href=""><img src="/youtube.png" alt="" /></a>
-                    <a href=""><img src="/dribbble.png" alt="" /></a>
+                    <a href="https://twitter.com/unknown_sarthak" target='_blank'><img src="/twitter_logo.png" alt="" /></a>
+                    <a href="https://www.instagram.com/vaish_sa_rthak/" target='_blank'><img src="/instagram.png" alt="" /></a>
+                    <a target='_blank' href="https://www.linkedin.com/in/sarthak-vaish-34a06321b/"><img src="/linkedin_logo.png" alt="" /></a>
+                    <a target='_blank' href="https://github.com/sarth-akvaish"><img src="/github_logo.jpg" alt="" style={{ backgroundColor: "white", objectFit: "cover" }} /></a>
                 </div>
             </div>
 
